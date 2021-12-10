e92a1e64a996d4b04dc0a1ebd3d9ff1efc29db02
Curtis Brooks <curtisbrooks678@gmail.com>
2021-12-10 10:35:08 -0800

    red fail for first test, string input to int output for BreadCost

--- PierresBakery.Tests/ModelTests/BakedGoodsTests.cs
+++ PierresBakery.Tests/ModelTests/BakedGoodsTests.cs
@@ -1,2 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Class.Models;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests 
+{
+  [TestClass]
+  public class BakedGoodsTests
+  {
+    [TestMethod]
+    public void BreadCost_ReturnsIntWithStringInput_0()
+    {
+      Assert.AreEqual(0, Bread.BreadCost("1"));
+    }
+  }
+}

d06a8eece65f86339354ce9c84846945a9405afe
Curtis Brooks <curtisbrooks678@gmail.com>
2021-12-10 15:56:14 -0800

    update for third test, pass, test for bread order amounts above 3 and not evenly divisible by 3

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -4,14 +4,21 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    public static int BreadCost(string input)
+    public static int BreadCost(string input) 
     {
       int totalCost = 0; 
       int intInput = int.Parse(input); 
       totalCost = intInput * 5; 
-      if (intInput % 3 == 0) 
+      if (intInput >= 3 && intInput % 3 == 0) 
       {
         int multOf3 = intInput / 3; 
+        totalCost -= (multOf3 * 5); 
+      } 
+      else if (intInput > 3 && intInput % 3 != 0)
+      {
+        int remainder = intInput % 3; 
+        intInput -= remainder; 
+        int multOf3 = intInput / 3;
         totalCost -= (multOf3 * 5); 
       }
       return totalCost; 
